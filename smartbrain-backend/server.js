5e7ef0437e4d68c353c4a68e0bf482dadc5c0fe3
Ruben Marin <staticparsley92@gmail.com>
2018-09-05 03:25:00 -0700

    updated backend to support db

--- smartbrain-backend/server.js
+++ smartbrain-backend/server.js
@@ -1,65 +1,36 @@
-const express = require('express')
-const app = express()
-var bodyParser = require('body-parser')
-var cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
 
-const database = {
-  users: [{
-    id: '123',
-    name: 'Andrei',
-    email: 'john@gmail.com',
-    entries: 0,
-    joined: new Date()
-  }],
-  secrets: {
-    users_id: '123',
-    hash: 'wghhh'
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'staticparslet',
+    password : '',
+    database : 'smartbrain'
   }
-}
+});
 
-app.use(cors());
+const app = express();
+
+app.use(cors())
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/signin', (req, res) => {
-  var a = JSON.parse(req.body);
-  if (a.username === database.users[0].email && a.password === database.secrets.hash) {
-    res.send('signed in');
-  } else {
-    res.json('access denied');
-  }
+app.get('/', (req, res)=> { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
 })
-
-app.post('/findface', (req, res) => {
-  database.users.forEach(user => {
-    if (user.email === req.body.email) {
-      user.entries++
-      res.json(user)
-    }
-  });
-  res.json('nope')
-})
-
-
-app.post('/register', (req, res) => {
-  database.users.push({
-    id: '124',
-    name: req.body.name,
-    email: req.body.email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length - 1])
-})
-
-app.get('/profile/:userId', (req, res) => {
-  database.users.forEach(user => {
-    if (user.id === req.params.userId) {
-      return res.json(user);
-    }
-  })
-  // res.json('no user')
-
-})
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
